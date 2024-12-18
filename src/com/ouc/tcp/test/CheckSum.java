3b218f003abf045ca60e817a74df9272a3140521
takuto <blct017@outlook.com>
2024-12-18 11:53:19 +0800

    rdt2.0,发包会出错，修改了checksum文件

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -6,14 +6,15 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-
     /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
     public static short computeChkSum(TCP_PACKET tcpPack) {
-        int checkSum = 0;
-
-
-
-        return (short) checkSum;
+        CRC32 checkSum = new CRC32();
+        TCP_HEADER header = tcpPack.getTcpH();//拿到头部
+        checkSum.update(header.getTh_seq());//校验seq字段
+        checkSum.update(header.getTh_ack());//校验ack字段
+        for (int i = 0; i < tcpPack.getTcpS().getData().length; i++ ){
+            checkSum.update(tcpPack.getTcpS().getData()[i]);//校验数据字段
+        }
+        return (short) checkSum.getValue();
     }
-
 }
--- src/com/ouc/tcp/test/TestRun.java
+++ src/com/ouc/tcp/test/TestRun.java
@@ -8,5 +8,6 @@ public class TestRun {
         SystemStart.main(null);
     }
 
+
 }
 
